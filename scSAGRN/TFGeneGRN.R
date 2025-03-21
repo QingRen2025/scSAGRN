64f01811424fa59fb143b206e965062b28046571
QingRen2025 <6231204012@stu.jiangnan.edu.cn>
2025-03-21 15:40:43 +0800

    Update TFGeneGRN.R

--- scSAGRN/TFGeneGRN.R
+++ scSAGRN/TFGeneGRN.R
@@ -1,21 +1,21 @@
-TFGeneGRN <-function (ATAC.se, dorcK = 30, dorcTab, n_bg = 50, genome, dorcMat, 
-          rnaMat, dorcGenes = NULL, nCores = 1) 
+TFGeneGRN <-function (WNNATAC, dorcK = 30, dorcTab, n_bg = 50, genome, dorcMat, 
+          WNNRNA, dorcGenes = NULL, nCores = 1) 
 {
-  stopifnot(all.equal(ncol(dorcMat), ncol(rnaMat)))
+  stopifnot(all.equal(ncol(dorcMat), ncol(WNNRNA)))
   if (!all(c("Peak", "Gene") %in% colnames(dorcTab))) 
     stop("Expecting fields Peak and Gene in dorcTab data.frame .. see runGenePeakcorr function in BuenRTools")
   if (all(grepl("chr", dorcTab$Peak, ignore.case = TRUE))) {
     usePeakNames <- TRUE
     message("Detected peak region names in Peak field")
-    if (!(all(grepl("chr", rownames(ATAC.se), ignore.case = TRUE)))) 
+    if (!(all(grepl("chr", rownames(WNNATAC), ignore.case = TRUE)))) 
       stop("Peak regions provided in dorcTab data.frame but not found as rownames in input SE")
-    if (!all(dorcTab$Peak %in% rownames(ATAC.se))) 
+    if (!all(dorcTab$Peak %in% rownames(WNNATAC))) 
       stop("Found DORC peak region not present in input SE.. make sure DORC calling output corresponds to same input SE as the one provided here ..")
   }
   else {
     usePeakNames <- FALSE
     message("Assuming peak indices in Peak field")
-    if (max(dorcTab$Peak) > nrow(ATAC.se)) 
+    if (max(dorcTab$Peak) > nrow(WNNATAC)) 
       stop("Found DORC peak index outside range of input SE.. make sure DORC calling output corresponds to same input SE as the one provided here ..")
   }
   if (is.null(dorcGenes)) {
@@ -34,14 +34,14 @@ TFGeneGRN <-function (ATAC.se, dorcK = 30, dorcTab, n_bg = 50, genome, dorcMat,
   DORC.knn <- FNN::get.knn(data = t(scale(Matrix::t(dorcMat))), 
                            k = dorcK)$nn.index
   rownames(DORC.knn) <- rownames(dorcMat)
-  if (is.null(SummarizedExperiment::rowData(ATAC.se)$bias)) {
+  if (is.null(SummarizedExperiment::rowData(WNNATAC)$bias)) {
     if (genome %in% "hg19") 
       myGenome <- BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19
     if (genome %in% "mm10") 
       myGenome <- BSgenome.Mmusculus.UCSC.mm10::BSgenome.Mmusculus.UCSC.mm10
     if (genome %in% "hg38") 
       myGenome <- BSgenome.Hsapiens.UCSC.hg38::BSgenome.Hsapiens.UCSC.hg38
-    ATAC.se <- chromVAR::addGCBias(ATAC.se, genome = myGenome)
+    WNNATAC <- chromVAR::addGCBias(WNNATAC, genome = myGenome)
   }
   packagePath <- find.package("FigR", lib.loc = NULL, quiet = TRUE)
   if (grepl("hg", genome)) {
@@ -53,29 +53,29 @@ TFGeneGRN <-function (ATAC.se, dorcK = 30, dorcTab, n_bg = 50, genome, dorcMat,
   if (all(grepl("_", names(pwm), fixed = TRUE))) 
     names(pwm) <- FigR::extractTFNames(names(pwm))
   message("Removing genes with 0 expression across cells ..\n")
-  rnaMat <- rnaMat[Matrix::rowSums(rnaMat) != 0, ]
-  myGeneNames <- gsub(x = rownames(rnaMat), pattern = "-", 
+  WNNRNA <- WNNRNA[Matrix::rowSums(WNNRNA) != 0, ]
+  myGeneNames <- gsub(x = rownames(WNNRNA), pattern = "-", 
                       replacement = "")
-  rownames(rnaMat) <- myGeneNames
+  rownames(WNNRNA) <- myGeneNames
   motifsToKeep <- intersect(names(pwm), myGeneNames)
   cat("Getting peak x motif matches ..\n")
-  motif_ix <- motifmatchr::matchMotifs(subject = ATAC.se, pwms = pwm[motifsToKeep], 
+  motif_ix <- motifmatchr::matchMotifs(subject = WNNATAC, pwms = pwm[motifsToKeep], 
                                        genome = genome)
   motif_ix <- motif_ix[, Matrix::colSums(assay(motif_ix)) != 
                          0]
   cat("Determining background peaks ..\n")
   cat("Using ", n_bg, " iterations ..\n\n")
-  if (any(Matrix::rowSums(assay(ATAC.se)) == 0)) {
-    ATAC.mat <- assay(ATAC.se)
+  if (any(Matrix::rowSums(assay(WNNATAC)) == 0)) {
+    ATAC.mat <- assay(WNNATAC)
     ATAC.mat <- cbind(ATAC.mat, 1)
-    ATAC.se.new <- SummarizedExperiment::SummarizedExperiment(assays = list(counts = ATAC.mat), 
-                                                              rowRanges = granges(ATAC.se))
+    WNNATAC.new <- SummarizedExperiment::SummarizedExperiment(assays = list(counts = ATAC.mat), 
+                                                              rowRanges = granges(WNNATAC))
     set.seed(123)
-    bg <- chromVAR::getBackgroundPeaks(ATAC.se.new, niterations = n_bg)
+    bg <- chromVAR::getBackgroundPeaks(WNNATAC.new, niterations = n_bg)
   }
   else {
     set.seed(123)
-    bg <- chromVAR::getBackgroundPeaks(ATAC.se, niterations = n_bg)
+    bg <- chromVAR::getBackgroundPeaks(WNNATAC, niterations = n_bg)
   }
   cat("Testing ", length(motifsToKeep), " TFs\n")
   cat("Testing ", nrow(dorcMat), " DORCs\n")
@@ -96,7 +96,7 @@ TFGeneGRN <-function (ATAC.se, dorcK = 30, dorcTab, n_bg = 50, genome, dorcMat,
       DORCNNpeaks <- unique(dorcTab$Peak[dorcTab$Gene %in% 
                                            c(g, rownames(dorcMat)[DORC.knn[g, ]])])
       if (usePeakNames) 
-        DORCNNpeaks <- which(rownames(ATAC.se) %in% DORCNNpeaks)
+        DORCNNpeaks <- which(rownames(WNNATAC) %in% DORCNNpeaks)
       mZ <- FigR::motifPeakZtest(peakSet = DORCNNpeaks, 
                                  bgPeaks = bg, tfMat = assay(motif_ix))
       mZ <- mZ[, c("gene", "z_test")]
@@ -106,7 +106,7 @@ TFGeneGRN <-function (ATAC.se, dorcK = 30, dorcTab, n_bg = 50, genome, dorcMat,
                                    lower.tail = FALSE)
       mZ$Enrichment.log10P <- sign(mZ$Enrichment.Z) * -log10(mZ$Enrichment.P)
       mZ <- cbind(DORC = g, mZ)
-      corr.r <- cor(dorcMat[g, ], t(as.matrix(rnaMat[mZ$Motif, 
+      corr.r <- cor(dorcMat[g, ], t(as.matrix(WNNRNA[mZ$Motif, 
       ])), method = "spearman")
       stopifnot(all.equal(colnames(corr.r), mZ$Motif))
       mZ$Corr <- corr.r[1, ]
