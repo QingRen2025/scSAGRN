763e866a5d95d3d17f5a721c126cf5842064201f
QingRen2025 <6231204012@stu.jiangnan.edu.cn>
2025-03-22 17:51:09 +0800

    Update grn.R

--- scSAGRN/grn.R
+++ scSAGRN/grn.R
@@ -6,54 +6,34 @@ library(BuenColors)
 library(FigR)
 library(BSgenome.Hsapiens.UCSC.hg38)
 
-RNA<-readRDS("method/pbmc/my/processdata/weighted_rna.rds")  
-ATAC<- readRDS("method/pbmc/my/processdata/weighted_atac.SE.rds")  
-dim(ATAC) # Peaks x ATAC cells
-dim(RNA) # Genes x RNA cells
-genelist <- read.table("method/pbmc/my/processdata/high_variable_genes.txt")
+pbmc <- readRDS("method/pbmc/processdata/pbmc__seurat_object.rds")  
+genelist <- read.table("method/pbmc/processdata/high_variable_genes.txt")
 
-cisCorr <- runGenePeakLink(WNNATAC = ATAC,
-                                 WNNRNA = RNA,
-                                 genome = "hg38", # One of hg19, mm10 or hg38 
-                                 geneList =genelist[[1]],
-                                 windowPadSize = 150000,
-                                 nCores = 8,
-                                 p.cut = NULL, # Set this to NULL and we can filter later
-                                 n_bg = 100)
-head(cisCorr)
-write.csv(cisCorr,file="method/pbmc/result/pbmc_peak_gene.csv",row.names=F)
+Peak_Gene <- runGenePeakLink(pbmc,
+                           genome = "hg38", 
+                           geneList =genelist[[1]],
+                           windowPadSize = 150000,
+                           nCores = 8,
+                           p.cut = NULL, 
+                           n_bg = 100)
+head(Peak_Gene)
+write.csv(Peak_Gene,file="method/pbmc/result/pbmc_peak_gene.csv",row.names=F)
 
-cisCorr <- read.csv("method/pbmc/result/pbmc_peak_gene.csv")
+#根据P值进行过滤
+Peak_Gene.filt <- Peak_Gene%>% filter(pvalZ <= 0.05)
+Gene <- unique(Peak_Gene.filt$Gene)
 
-
-cisCorr.filt <- cisCorr%>% filter(pvalZ <= 0.05)
-
-dorcGenes <- GeneRankPlot(dorcTab = cisCorr.filt,
-                        cutoff = 1, # No. sig peaks needed to be called a DORC
-                        labelTop = 20,
-                        returnGeneList = TRUE, # Set this to FALSE for just the plot
-                        force=2)
-
-
-# Unfiltered 获得基因名称
-numDorcs <- cisCorr.filt %>% group_by(Gene) %>% tally() %>% arrange(desc(n))
-numDorcs
-
-dorcMat <- getDORCScores(WNNATAC = ATAC,                          
-                         dorcTab = cisCorr.filt,                         
-                         geneList = dorcGenes,                         
+GeneScore <- runGeneScore(A549,                          
+                         peakgene = Peak_Gene.filt,                         
+                         geneList = Gene,                         
                          nCores = 4)
-dim(dorcMat)
 
-saveRDS(dorcMat, file = "method/pbmc/result/dorcMat.rds")
-# 使用readRDS函数加载RDS文件  
-dorcMat <- readRDS(file = "method/pbmc/result/dorcMat.rds")
+saveRDS(GeneScore, file = "method/pbmc/result/GeneScore.rds")
 
-TF_Gene <- TFGeneGRN(WNNATAC = ATAC, # Must be the same input as used in runGenePeakcorr()
-                     dorcTab = cisCorr.filt, # Filtered peak-gene associations
+TF_Gene <- TFGeneGRN(A549, 
+                     peakgene = Peak_Gene.filt, 
                      genome = "hg38",
-                     dorcMat = dorcMat,
-                     WNNRNA = RNA, 
+                     GeneScore = GeneScore,
                      nCores = 4)
-# 保存 TF_Gene 对象
+
 saveRDS(TF_Gene, file = "method/pbmc/result/TF_Generesults.rds")
